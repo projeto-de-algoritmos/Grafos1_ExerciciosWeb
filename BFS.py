0f2e4fec039ab92e85566ae1f733b3aa66191502
caiozim112 <caioopua@gmail.com>
2023-04-27 18:56:32 -0300

    chore: add ordem topologica

--- BFS.py
+++ BFS.py
@@ -36,9 +36,5 @@ if __name__ == '__main__':
     g.addEdge(2, 0)
     g.addEdge(2, 3)
     g.addEdge(3, 3)
- 
-    print("Following is Breadth First Traversal"
-          " (starting from vertex 2)")
+
     g.BFS(2)
- 
-# This code is contributed by Neelam Yadav
---
+++ OrdemTopologica.py
@@ -0,0 +1,39 @@
+from collections import deque
+import pytest
+def ordenacao_topologica(grafo):
+    # calcular grau de entrada para cada vértice
+    grau_entrada = {v: 0 for v in grafo}
+    for v in grafo:
+        for u in grafo[v]:
+            grau_entrada[u] += 1
+    # colocar vértices sem arestas de entrada na fila
+    fila = deque(v for v in grafo if grau_entrada[v] == 0)
+    # ordenação topológica
+    resultado = []
+    while fila:
+        v = fila.popleft()
+        resultado.append(v)
+     # busca menor grau
+        for u in grafo[v]:
+            grau_entrada[u] -= 1
+            if grau_entrada[u] == 0:
+                fila.append(u)
+    
+    # verificar se todos os vértices foram visitados
+    if len(resultado) != len(grafo):
+        raise ValueError("Grafo possui ciclo")
+    
+    return resultado
+
+if __name__ == '__main__':
+
+    grafo = {
+        'v1': ['v5', 'v7'],
+        'v2': ['v3', 'v5', 'v6'],
+        'v3': ['v4','v5'],
+        'v4': ['v5'],
+        'v5': ['v6','v7'],
+        'v6': ['v7'],
+        'v7': []
+    }
+    print(ordenacao_topologica(grafo))
